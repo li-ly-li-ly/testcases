353080ff339272c8b35ee66db9e91cca264e3106
lily <1210402880@qq.com>
2021-09-17 16:52:29 +0800

    上传测试案例

---
+++ test/test_cases.py
@@ -0,0 +1,27 @@
+import unittest
+import logging
+logging.basicConfig(level=logging.INFO)
+class Mytest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        logging.info('setipcalsss')
+    @classmethod
+    def tearDownClass(cls) -> None:
+        logging.info('trear down class')
+    def setUp(self) -> None:
+        logging.info('开始')
+    def tearDown(self) -> None:
+        logging.info('结束')
+    def test_01(self):
+        logging.info('第一个测试用例执行')
+        self.assertEqual(1,2,'判断相等')
+
+    def test_02(self):
+        logging.info('第一个测试用例执行')
+        self.assertEqual(2, 2, '判断相等')
+    @unittest.skip
+    def test_03(self):
+        logging.info('第一个测试用例执行')
+        self.assertEqual(2, 5, '判断相等')
+if __name__ == '__main__':
+    unittest.main()
